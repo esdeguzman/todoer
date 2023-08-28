e598971c0c4c12b1e197892bd604ca2e0b32a6bd
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2023-08-28 09:22:28 +0800

    Added handling for no todos yet

--- src/pages/components/Todos.jsx
+++ src/pages/components/Todos.jsx
@@ -1,23 +1,42 @@
+import { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
-function Todos({ todos, completeTodo, deleteTodo, hideTodo, setTitle, setDescription }) {
-  return todos
-    .slice()
-    .reverse()
-    .map((todo) => (
-      <TodoItem
-        key={todo.id}
-        id={todo.id}
-        title={todo.title}
-        description={todo.description}
-        completed={todo.completed}
-        completeTodo={completeTodo}
-        deleteTodo={deleteTodo}
-        hideTodo={hideTodo}
-        setTitle={setTitle}
-        setDescription={setDescription}
-      />
-    ));
+function Todos({
+  filteredTodos,
+  completeTodo,
+  deleteTodo,
+  hideTodo,
+  setTitle,
+  setDescription,
+}) {
+  return (
+    <>
+      {filteredTodos.length > 0 ? (
+        filteredTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            description={todo.description}
+            completed={todo.completed}
+            hidden={todo.hidden}
+            completeTodo={completeTodo}
+            deleteTodo={deleteTodo}
+            hideTodo={hideTodo}
+            setTitle={setTitle}
+            setDescription={setDescription}
+          />
+        ))
+      ) : (
+        <div className="no-todos">
+          <div className="text-xl font-semibold">💭 No Todos yet 💭</div>
+          <div className="text-xl font-semibold">
+            🔥 Use the form above and start your productive day! 🔥
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Todos;
